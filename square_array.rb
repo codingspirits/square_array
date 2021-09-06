757eb7800403040c5ff2deaabec76ecff07d394a
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-06 11:26:54 +1000

    Completed Assignment

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+numbers = [1,2,3]
+
 def square_array(array)
-  # your code here
+  squared = []
+  array.each { |element| squared.push(element * element)}
+  squared
 end
+
+p square_array(numbers)
+
